ce2e9393ce7298009969c770c68465fae1e7f85a
miros <slachta1@stud.uniza.sk>
2023-01-09 11:41:38 +0100

    druha cast

--- C/matrix.c
+++ C/matrix.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+//matrix
 char ** mat2;
 char** alokujMatrix(){
     mat2=(char**) calloc(8,sizeof(char*));
--- H/matrix.h
+++ H/matrix.h
@@ -1,7 +1,7 @@
 //
 // Created by miros on 06/01/2023.
 //
-
+//matrix
 #ifndef SKUSKA_CLIENT_SERVER2_MATRIX_H
 #define SKUSKA_CLIENT_SERVER2_MATRIX_H
 char** alokujMatrix();
