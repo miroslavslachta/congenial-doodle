040c30ed132f480a4dedd479bc80d43bb9e1ba67
JakubZajac214 <j.zajac214@gmail.com>
2023-01-09 12:14:10 +0100

    MakeFile

---
+++ MakeFile.c
@@ -0,0 +1,19 @@
+CC=gcc
+CFLAGS=-c -Wall -pthread
+
+all: main
+
+        main: main.o server.o client.o matrix.o
+$(CC) main.o server.o client.o matrix.o -o main -pthread
+
+        main.o: main.c
+$(CC) $(CFLAGS) main.c
+
+        server.o: server.c server.h
+$(CC) $(CFLAGS) server.c
+
+        client.o: client.c client.h
+$(CC) $(CFLAGS) client.c
+
+        matrix.o: matrix.c matrix.h
+$(CC) $(CFLAGS) matrix.c
